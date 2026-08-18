4d33d8f10f059d1df62716c893307bb79dceefe6
minghuitang <tmh2818@163.com>
2026-08-18 15:20:40 +0800

    学习创建类

---
+++ Python_zimu/chat9面向对象程序设计/示例9-002自定义数据类型.py
@@ -0,0 +1,23 @@
+# 类
+# 编写一个Person 类型
+class Person():
+    pass
+
+# 编写一个Cat 类型
+class Cat():
+    pass
+
+# 编写一个Dog 类型
+class Dog():
+    pass
+
+# 编写一个Student 类型
+class Student():
+    pass
+
+class StudentPerson(Cat, Dog):
+    pass
+
+# 对类名 后面的括号 省略
+class Pig:
+    pass
